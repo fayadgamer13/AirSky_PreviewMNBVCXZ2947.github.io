2d03811ac3f3aa2e04edc96e52788d2faaa3a20f
fayadgamer13 <wadifayad@gmail.com>
2026-09-24 16:52:27 +1000

    Update website URL in script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const button = document.getElementById("newWebsite");
 
 // Change this to the URL of your newer website.
-const newerWebsite = "https://example.com";
+const newerWebsite = "https://fayadgamer13.github.io/AirSky.github.io";
 
 button.href = newerWebsite;
 
